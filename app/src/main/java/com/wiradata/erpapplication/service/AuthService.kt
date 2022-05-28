29a2410fbf4222d94c9576d00d51746d0383dfa9
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2022-05-28 19:48:12 +0700

    fix add permission internet

--- app/src/main/java/com/wiradata/erpapplication/service/AuthService.kt
+++ app/src/main/java/com/wiradata/erpapplication/service/AuthService.kt
@@ -6,19 +6,28 @@ import com.wiradata.erpapplication.GrpcService
 import com.wiradata.erpapplication.users.AuthServiceGrpc
 import com.wiradata.erpapplication.users.LoginRequest
 import com.wiradata.erpapplication.users.LoginResponse
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 class AuthService(context: Context) {
     private lateinit var stub: AuthServiceGrpc.AuthServiceBlockingStub
 
     fun login(user: String, password: String): LoginResponse {
-        val chanel = GrpcService().createMangagedChanel()
-        stub = AuthServiceGrpc.newBlockingStub(chanel)
-        val request =
-            LoginRequest.newBuilder().setUsername(user).setPassword(password).build()
-        var response : LoginResponse = stub.login(request)
-        Log.i("adit :", response.toString())
-        chanel.awaitTermination(5, TimeUnit.SECONDS);
-        return response
+        try {
+            val chanel = GrpcService().createMangagedChanel()
+            stub = AuthServiceGrpc.newBlockingStub(chanel)
+            val request =
+                LoginRequest.newBuilder().setUsername(user).setPassword(password).build()
+            var response : LoginResponse = stub.login(request)
+            Log.i("adit :", response.toString())
+            chanel.awaitTermination(5, TimeUnit.SECONDS);
+            return response
+        }
+        catch (e : IOException) {
+            Log.i("adit :", "io cause exception " + e.cause)
+        } catch (e : Exception){
+            Log.i("adit :", "cause exception " + e.cause)
+        }
+        return LoginResponse.newBuilder().build()
     }
 }
